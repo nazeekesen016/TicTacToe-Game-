7bb459f23a563a2fa2f2431d4ced9388cc9bacd4
nazeekesen016 <60453904+nazeekesen016@users.noreply.github.com>
2020-11-13 19:28:48 +0600

    Add files via upload

--- TicTacToe/src/main/java/Main.java
+++ TicTacToe/src/main/java/Main.java
@@ -3,6 +3,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import java.io.*;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Main extends Application {
     @Override
@@ -23,4 +26,36 @@ public class Main extends Application {
         launch(args);
     }
 }
+
+class Write {
+
+    public static void main(String[] args) {
+        try {
+            BufferedWriter bw = new BufferedWriter(
+                    new FileWriter("C:\\Users\\Nazik Esengulova\\IdeaProjects\\TicTacToe\\text\\output.txt"));
+            bw.write("Program is succesfully running");
+            bw.close();
+        } catch (Exception ex) {
+            return;
+        }
+    }
+}
+
+    class Read {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(
+                    new FileReader("C:\\Users\\Nazik Esengulova\\IdeaProjects\\TicTacToe\\text\\output.txt"));
+            String s;
+            while(( s = br.readLine()) != null){
+                System.out.println(s);
+            }
+            br.close();
+        } catch (Exception ex) {
+            return;
+        }
+    }
+}
+
 
